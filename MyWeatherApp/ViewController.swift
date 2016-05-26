976c77c8880f5afa491a20ad56129bec717ff687
Wu Binbin <wubinbin@WudeMac.local>
2016-05-26 23:39:04 +0800

    finished Location and Location Label display

--- MyWeatherApp/ViewController.swift
+++ MyWeatherApp/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import Foundation
+import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    //MARK:@IBOut
+    @IBOutlet weak var UserLocationLabel: UILabel!
+    
+    var seenError : Bool = false
+    var locationFixAchieved : Bool = false
+    var locationStatus : NSString = "Not Started"
+    var locationManager: CLLocationManager!
+    var userLocation : String!
+    var userLatitude : Double!
+    var userLongitude : Double!
+    var userTemperatureCelsius : Bool!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initLocationManager()
     }
-
+    func initLocationManager() {
+        seenError = false
+        locationFixAchieved = false
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+    }
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error)->Void in
+            
+            let pm = placemarks![0]
+            self.displayLocationInfo(pm)
+        })
+        
+        if (locationFixAchieved == false) {
+            locationFixAchieved = true
+            var locationArray = locations as NSArray
+            var locationObj = locationArray.lastObject as! CLLocation
+            var coord = locationObj.coordinate
+            self.userLatitude = coord.latitude
+            self.userLongitude = coord.longitude
+            
+            //getCurrentWeatherData()
+            
+            
+        }
+    }
+    func displayLocationInfo(placemark: CLPlacemark?) {
+        if let containsPlacemark = placemark {
+            //stop updating location to save battery life
+            locationManager.stopUpdatingLocation()
+            let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality : ""
+            let postalCode = (containsPlacemark.postalCode != nil) ? containsPlacemark.postalCode : ""
+            let administrativeArea = (containsPlacemark.administrativeArea != nil) ? containsPlacemark.administrativeArea : ""
+            let country = (containsPlacemark.country != nil) ? containsPlacemark.country : ""
+            print(locality)
+            print(postalCode)
+            print(administrativeArea)
+            print(country)
+            
+            self.UserLocationLabel.text = locality
+            
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager,
+                         didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        var shouldIAllow = false
+        
+        switch status {
+        case CLAuthorizationStatus.Restricted:
+            locationStatus = "Restricted Access to location"
+        case CLAuthorizationStatus.Denied:
+            locationStatus = "User denied access to location"
+        case CLAuthorizationStatus.NotDetermined:
+            locationStatus = "Status not determined"
+        default:
+            locationStatus = "Allowed to location Access"
+            shouldIAllow = true
+        }
+        NSNotificationCenter.defaultCenter().postNotificationName("LabelHasbeenUpdated", object: nil)
+        if (shouldIAllow == true) {
+            NSLog("Location to Allowed")
+            // Start location services
+            locationManager.startUpdatingLocation()
+        } else {
+            NSLog("Denied access: \(locationStatus)")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
