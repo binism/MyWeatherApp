ddc67a8b46b3742ab0d5269584ef0378e387f704
Wu Binbin <wubinbin@WudeMac.local>
2016-05-29 17:01:50 +0800

    Linked main.storyboard to ViewController

--- MyWeatherApp/ViewController.swift
+++ MyWeatherApp/ViewController.swift
@@ -10,8 +10,75 @@ import UIKit
 import Foundation
 import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    //MARK:@IBOut
+    //MARK:@IBOut Today
+    //UserLocation
     @IBOutlet weak var UserLocationLabel: UILabel!
+    
+    //SummaryStackView
+    @IBOutlet weak var SummaryIcon: UIImageView!
+    @IBOutlet weak var SummaryLabel: UILabel!
+    
+    //CurrentTemrature
+    @IBOutlet weak var CurrentTempratureLabel: UILabel!
+    
+    //TodayHighTempStackView
+    @IBOutlet weak var TodayHighTemperature: UILabel!
+    @IBOutlet weak var TodayHighTemperatureLabel: UILabel!
+    
+    //TodayLowTempStackView
+    @IBOutlet weak var TodayLowTemperature: UILabel!
+    @IBOutlet weak var TodayLowTemperatureLabel: UILabel!
+    
+    //HeatIndexImg
+    @IBOutlet weak var HeatIndexImg: UIImageView!
+    
+    //windbag&WindStackView
+    @IBOutlet weak var windbagImg: UIImageView!
+    @IBOutlet weak var WindIndex: UILabel!
+    @IBOutlet weak var WindLabel: UILabel!
+    
+    //umbrella&RainStackView
+    @IBOutlet weak var umbrellaImg: UIImageView!
+    @IBOutlet weak var RainIndex: UILabel!
+    @IBOutlet weak var RainLabel: UILabel!
+    
+    //humidity&HumidityStackView
+    @IBOutlet weak var humidityImg: UIImageView!
+    @IBOutlet weak var HumidityIndex: UILabel!
+    @IBOutlet weak var HumidityLabel: UILabel!
+    
+    
+    //MARK:@IBOutlet weekday
+    
+    //1stDayStackView
+    @IBOutlet weak var DayOneWeekDay: UILabel!
+    @IBOutlet weak var DayOneImg: UIImageView!
+    @IBOutlet weak var DayOneHighLowTemp: UILabel!
+    
+    //2ndDayStackView
+    @IBOutlet weak var DayTwoWeekDay: UILabel!
+    @IBOutlet weak var DayTwoImg: UIImageView!
+    @IBOutlet weak var DayTwoHighLowTemp: UILabel!
+    
+    //3rdDayStackView
+    @IBOutlet weak var DayThreeWeekDaty: UILabel!
+    @IBOutlet weak var DayThreeImg: UIImageView!
+    @IBOutlet weak var DayThreeHighLowTemp: UILabel!
+    
+    //4thDayStackView
+    @IBOutlet weak var DayFourWeekDay: UILabel!
+    @IBOutlet weak var DayFourImg: UIImageView!
+    @IBOutlet weak var DayFourHignLowTemp: UILabel!
+    
+    //5thDayStackView
+    @IBOutlet weak var DayFiveWeekDay: UILabel!
+    @IBOutlet weak var DayFiveImg: UIImageView!
+    @IBOutlet weak var DayFiveHighLowTemp: UILabel!
+    
+    //6thDayStackView
+    @IBOutlet weak var DaySixWeekDay: UILabel!
+    @IBOutlet weak var DaySixImg: UIImageView!
+    @IBOutlet weak var DaySixHighLowTemp: UILabel!
     
     var seenError : Bool = false
     var locationFixAchieved : Bool = false
